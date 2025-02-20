d206ecb618115b9eb0daae9514898f4756154065
pedrogomes107 <pedromcgomes95@gmail.com>
2025-02-20 21:07:40 +0000

    Fetch Data from Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <h1>Vite + React</h1>;
+  const API_key = "8bd6f64b95fe345767d440fd333532b2";
+
+  const search = async () => {
+    let city = "London";
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_key}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  };
+
+  return <h1 onClick={search}>Hello World</h1>;
 }
 
 export default App;
